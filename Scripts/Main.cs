1f9bbf0c518b55e9fb9031335315e8dfc9fe1a5d
James Lewis <jamesblewislewis@gmail.com>
2025-02-12 17:25:16 +1100

    added map

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,14 +3,14 @@ using System;
 
 public class Main : Node2D
 {
-    public override void _Ready()
-    {
-        GD.Print("Game started!");
-    }
+	public override void _Ready()
+	{
+		GD.Print("Game started!");
+	}
 
-    public override void _Process(float delta)
-    {
-        GD.Print("Game loop!");
-        // Game loop logic here
-    }
+	public override void _Process(float delta)
+	{
+		GD.Print("Game loop!");
+		// Game loop logic here
+	}
 }
